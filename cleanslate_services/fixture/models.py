79e0db2056407f077328ce61d37e2bd881eb284e
Archit Rastogi <g2rastogi@gmail.com>
2023-04-18 17:02:04 +0530

    Add method to create and list test session

--- cleanslate_services/fixture/models.py
+++ cleanslate_services/fixture/models.py
@@ -9,6 +9,30 @@ FIXTURE_STATUS = [
 ]
 
 
+class TestSessionStatus(models.IntegerChoices):
+
+    """Status for a test session."""
+
+    CREATED = 1
+    STARTING = 2
+    STARTED = 3
+    FINISHING = 4
+    FINISHED = 5
+
+
+class FixtureInstanceStatus(models.IntegerChoices):
+
+    """Status for a test session."""
+
+    CREATED = 1
+    SETUP_RUNNING = 2
+    SETUP_FINISHED = 3
+    SETUP_FAILED = 4
+    TEARDOWN_RUNNING = 5
+    TEARDOWN_FINISHED = 6
+    TEARDOWN_FAILED = 7
+
+
 class FixtureDefs(models.Model):
 
     """Fixture model to capture a remote fixture."""
@@ -21,3 +45,39 @@ class FixtureDefs(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
     status = models.CharField(max_length=1, choices=FIXTURE_STATUS)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+
+class TestSession(models.Model):
+
+    """A test session where all tests run."""
+
+    class Meta:
+        db_table = "cleanslate_test_session"
+        abstract = False
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200)
+    status = models.IntegerField(choices=TestSessionStatus.choices)
+    exit_code = models.IntegerField(null=True)
+    is_deleted = models.BooleanField(default=False)
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+
+
+class FixtureInstance(models.Model):
+
+    """A running Fixture instance."""
+
+    class Meta:
+        db_table = "cleanslate_fixture_instances"
+        abstract = False
+
+    fixture_def_id = models.ForeignKey(FixtureDefs, on_delete=models.PROTECT)
+    session_id = models.ForeignKey(TestSession, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=FixtureInstanceStatus.choices)
+    message = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
--- cleanslate_services/fixture/views/crud.py
+++ cleanslate_services/fixture/views/crud.py
@@ -1,11 +1,21 @@
 """All views are defined here. Implementation must go to lib."""
-from typing import List
+import json
+import logging
+from datetime import datetime
+from typing import List, Dict, Any
 
-import fixture.lib.api as api_lib
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.forms.models import model_to_dict
 from django.http import HttpRequest, JsonResponse
 from django.views.decorators.csrf import requires_csrf_token
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
+
+import fixture.lib.api as api_lib
+from fixture.models import TestSession, TestSessionStatus
+from fixture.views.models import TestSessionRequest
+
+LOGGER = logging.getLogger(__name__)
 
 
 @login_required
@@ -14,3 +24,52 @@ from django.views.decorators.http import require_GET
 def fixture_list(request: HttpRequest) -> List[str]:
     """List all known fixtures."""
     return JsonResponse(data={"data": api_lib.list_fixtures()})
+
+
+@login_required
+@requires_csrf_token
+@require_POST
+def create_test_session(request: HttpRequest):
+    """Create a new test session."""
+    req_body: Dict[str, Any] = json.loads(request.body)
+
+    # validate the request body
+    session_req = TestSessionRequest(**req_body)
+
+    # create a new test session
+    new_session = TestSession(
+        name=session_req.name,
+        description=session_req.description,
+        status=TestSessionStatus.CREATED,
+        created_at=datetime.now(),
+        updated_at=None,
+        exit_code=None,
+        is_deleted=False,
+        message=None)
+    new_session.save()
+    LOGGER.debug("Created new test session: %s", new_session.id)
+    return JsonResponse(data=model_to_dict(new_session))
+
+
+@login_required
+@requires_csrf_token
+@require_GET
+def list_test_sessions(request: HttpRequest):
+    """List test sessions."""
+    all_sessions = TestSession.objects.all().filter(is_deleted=False)
+    LOGGER.debug("Found %d active sessions.", len(all_sessions))
+    session_dict = {
+        "data": [model_to_dict(session) for session in all_sessions]
+    }
+    return JsonResponse(data=session_dict)
+
+def create_fixture_instance(request: HttpRequest):
+    """Create a new fixture instance."""
+
+
+def acquire_fixture_instance(request: HttpRequest):
+    """Acquires a lock on fixture instance."""
+
+
+def yield_fixture_instance(request: HttpRequest):
+    """Yields the lock on fixture to be later acquired by other processes."""
