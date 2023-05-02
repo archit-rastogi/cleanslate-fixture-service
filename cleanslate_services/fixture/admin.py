9c28d90cf531422442e27f6469112cbcfe485124
Archit Rastogi <g2rastogi@gmail.com>
2023-05-02 18:41:48 +0530

    Add objects to admin for testing purposes

--- cleanslate_services/fixture/admin.py
+++ cleanslate_services/fixture/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin  # noqa
 
+from .models import Session, FixtureDefs, FixtureInstance, Resource
+
 # Register your models here.
+admin.site.register(Session)
+admin.site.register(FixtureDefs)
+admin.site.register(FixtureInstance)
+admin.site.register(Resource)
